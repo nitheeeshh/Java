cbb7dfa39c0dcd0a9e4ed32f12b4d4a6ec006416
nitheeeshh <nitheeshk889@gmail.com>
2026-03-31 11:28:23 +0530

    New

---
+++ h1.java
@@ -0,0 +1,42 @@
+class Java 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("Welcome to Java Programming!");
+    }
+}
+class Food 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("i love biriyani");
+    }
+}
+class Dream 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("Java Sundaresan");
+    }
+}
+class Music 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("Life is better with music!");
+    }
+}
+class Canteen 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("Samosa and chai ready!");
+    }
+}
+class ExamHall 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("No talking, only writing!");
+    }
+}
---
+++ h2.java
@@ -0,0 +1,35 @@
+class Cricket 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("Dhoni finishes off in style!");
+    }
+}
+class CoffeeShop 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("One strong filter coffee");
+    }
+}
+class BankATM 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("Balance: ₹10 Mic off, video off, sleeping ON!");
+    }
+}
+class OnlineClass 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("As always Boring,Just Kidding :) ");
+    }
+}
+class IplFinals 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("Ee sala cup namdhe");
+    }
+}
