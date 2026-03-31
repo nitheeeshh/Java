e09ef9f84ffdd969842fee2ebd7ef010dda22a55
nitheeeshh <nitheeshk889@gmail.com>
2026-03-31 19:47:18 +0530

    Name

---
+++ h3.java
@@ -0,0 +1,70 @@
+class Greeting  
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("Good Morning");
+    }
+}
+class Evening 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("Good Evening");
+    }
+}
+class Night 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("Good Night");
+    }
+}
+class Java 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("Java is powerful");
+    }
+}
+class Practice 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("Practice makes perfect");
+    }
+}
+class Success 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("Success is yours");
+    }
+}
+class Learn 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("Keep Learning");
+    }
+}
+class Code 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("Coding is fun");
+    }
+}
+class Future 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("Future Developer");
+    }
+}
+class Aim 
+{
+      public static void main(String[]args) 
+    {
+        System.out.println("Achieve your goals");
+    }
+}
