c348ad68ad0bf0176a2305e0f828e244fb735502
nitheeeshh <nitheeshk889@gmail.com>
2026-04-01 20:09:13 +0530

    https://github.com/nitheeeshh/Java.git

---
+++ q1.java
@@ -0,0 +1,14 @@
+class Samp
+{
+  public static void main (String []args)
+  {
+   String name="Hari";
+   int age=17;
+   char grade='B';
+   boolean pass=true;
+   System.out.println("Name: "+name);
+   System.out.println("Age: "+age);
+   System.out.println("Grade: "+grade);
+   System.out.println("PASSED: "+pass);
+  }
+}  
---
+++ q3.java
@@ -0,0 +1,22 @@
+class Samp
+{
+  public static void main (String []args)
+  {
+   float weight=80.5f;
+   String name="Nitheesh";
+   byte age=22;
+   char gender='M';
+   boolean job=false;
+   long numb=9876543210l;
+   double height=170.5;
+   short dobyr=2003;
+   System.out.println("Name: "+name);
+   System.out.println("Age: "+age);
+   System.out.println("Gender: "+gender);
+   System.out.println("Job Status: "+job);
+   System.out.println("Mobile no: "+numb);
+   System.out.println("Height: "+height);
+   System.out.println("Weight: "+weight);
+   System.out.println("DOB year: "+dobyr);
+  }
+}  
