7a69a42ebdb25f548f42e5e623277d0bf26e80e2
nitheeeshh <nitheeshk889@gmail.com>
2026-04-06 21:08:10 +0530

    https://github.com/nitheeeshh/Java.git

---
+++ 6-4-26/Arth.java
@@ -0,0 +1,18 @@
+class Arth
+{
+	public static void main(String []arg)
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		int d=a-b;
+		int e=a*b;
+		int f=b%a;
+		int g=b/a;
+		System.out.println("a+b= "+c);
+		System.out.println("a-b= "+d);
+		System.out.println("a*b= "+e);
+		System.out.println("b%a= "+f);
+		System.out.println("b/a= "+g);
+	}
+}
---
+++ 6-4-26/Rela.java
@@ -0,0 +1,14 @@
+class Rela
+{
+	public static void main(String []arg)
+	{
+        int a = 10;
+        int b = 20;
+        System.out.println(a == b); 
+        System.out.println(a != b);  
+        System.out.println(a > b);   
+        System.out.println(a < b); 
+        System.out.println(a >= b);  
+        System.out.println(a <= b); 
+    }
+}
