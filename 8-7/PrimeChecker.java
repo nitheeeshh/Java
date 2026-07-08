0cc67ca2cc6a41f2cd6a6636f667ad2fc4a2e0ff
nitheeeshh <nitheeshk889@gmail.com>
2026-07-08 21:44:53 +0530

    https://github.com/nitheeeshh/Java.git

---
+++ 8-7/PrimeChecker.java
@@ -0,0 +1,34 @@
+package javaapplication59;
+
+import java.util.Scanner;
+
+public class PrimeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        if (isPrime(number)) {
+            System.out.println(number + " is a Prime Number");
+        } else {
+            System.out.println(number + " is not a Prime Number");
+        }
+        
+        scanner.close();
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        
+        for (int i = 2; i <= n / i; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
